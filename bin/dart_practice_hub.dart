e54d42462d0e7e6903c82d28417157a28719f75a
HossainNur <nurhossainsani1@gmail.com>
2025-08-04 17:09:05 +0600

    map added

--- bin/dart_practice_hub.dart
+++ bin/dart_practice_hub.dart
@@ -76,10 +76,25 @@ void main(List<String> arguments) {
     print(i);
   }*/
 
-  // input
-  var i = 20;
+  // // input
+  // var i = 20;
+  //
+  // print('fibonacci($i) = ${fibonacci(i)}');
 
-  print('fibonacci($i) = ${fibonacci(i)}');
+  // Creating Map using Constructors
+  var gfg = new Map();
+
+  // Inserting values into Map
+  gfg [0] = 'Geeks';
+  gfg [1] = 'for';
+  gfg [2] = 'Geeks';
+
+  // Printing Its content
+  print(gfg);
+
+  // Printing Specific Content
+  // Key is defined
+  print(gfg[0]);
 
 }
 
